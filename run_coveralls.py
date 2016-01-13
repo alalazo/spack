adc69249b7567070adcb4811b008a8e912e97533
alalazo <massimiliano.culpo@googlemail.com>
2016-01-13 18:40:51 +0100

    tox : don't set things globally when you can do it locally!

---
+++ run_coveralls.py
@@ -0,0 +1,12 @@
+#!/bin/env/python
+
+import os
+
+from subprocess import call
+
+
+if __name__ == '__main__':
+    if 'TRAVIS' in os.environ:
+        print "RUNNING COVERALLS"
+        rc = call('coveralls')
+        raise SystemExit(rc)
