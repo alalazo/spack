5bccd069af6b7f0533edf74d667a421b62e3b25d
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2022-11-11 23:40:55 +0100

    architecture.py: use new markers

--- lib/spack/spack/test/architecture.py
+++ lib/spack/spack/test/architecture.py
@@ -186,6 +186,7 @@ def test_satisfy_strict_constraint_when_not_concrete(architecture_tuple, constra
     assert not architecture.satisfies(constraint, strict=True)
 
 
+@pytest.mark.only_clingo("Fixing the parser broke this test for the original concretizer")
 @pytest.mark.parametrize(
     "root_target_range,dep_target_range,result",
     [
@@ -204,12 +205,6 @@ def test_satisfy_strict_constraint_when_not_concrete(architecture_tuple, constra
 def test_concretize_target_ranges(root_target_range, dep_target_range, result, monkeypatch):
     # Monkeypatch so that all concretization is done as if the machine is core2
     monkeypatch.setattr(spack.platforms.test.Test, "default", "core2")
-
-    # use foobar=bar to make the problem simpler for the old concretizer
-    # the new concretizer should not need that help
-    if spack.config.get("config:concretizer") == "original":
-        pytest.skip("Fixing the parser broke this test for the original concretizer.")
-
     spec_str = "a %%gcc@10 foobar=bar target=%s ^b target=%s" % (
         root_target_range,
         dep_target_range,
