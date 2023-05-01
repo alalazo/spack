3744e457dff1df0ff4fd2b7274b5e05705a8e08d
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2023-05-01 18:30:58 +0200

    Remove support for Ubuntu 18.04

--- lib/spack/spack/container/images.py
+++ lib/spack/spack/container/images.py
@@ -36,7 +36,7 @@ def build_info(image, spack_version):
 
     Args:
         image (str): image to be used at run-time. Should be of the form
-            <image_name>:<image_tag> e.g. "ubuntu:18.04"
+            <image_name>:<image_tag> e.g. "ubuntu:22.04"
         spack_version (str): version of Spack that we want to use to build
 
     Returns:
@@ -61,7 +61,7 @@ def os_package_manager_for(image):
 
     Args:
         image (str): image to be used at run-time. Should be of the form
-            <image_name>:<image_tag> e.g. "ubuntu:18.04"
+            <image_name>:<image_tag> e.g. "ubuntu:22.04"
 
     Returns:
         Name of the package manager, e.g. "apt" or "yum"
